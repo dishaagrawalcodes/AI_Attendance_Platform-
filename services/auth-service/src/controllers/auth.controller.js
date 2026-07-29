41844910f8b5594b5e8e87503226e5244814d6f3
Disha Agrawal <dishaagrawal9568@gmail.com>
2026-07-29 18:34:26 +0530

    auth route,controller,middleware

---
+++ services/auth-service/src/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User Registered Successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  register,
+};
--- services/auth-service/src/routes/auth.routes.js
+++ services/auth-service/src/routes/auth.routes.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+const { register } = require("../controllers/auth.controller");
+
 router.get("/health", (req, res) => {
-    res.json({
-        success: true,
-        message: "Auth Service is Healthy"
-    });
+  res.json({
+    success: true,
+    message: "Auth Service is Healthy",
+  });
 });
+
+router.post("/register", register);
 
 module.exports = router;
