67df78934dd485ab302e2e98aea194356522a74f
Disha Agrawal <dishaagrawal9568@gmail.com>
2026-07-29 18:34:54 +0530

    server file

--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -1,15 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-const PORT = 5000;
+connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Auth Service running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
