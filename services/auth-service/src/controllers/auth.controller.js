f1f9b696625b6f252d9be924a704f0a213cc3517
Disha Agrawal <dishaagrawal9568@gmail.com>
2026-07-30 16:07:39 +0530

    controller

--- services/auth-service/src/controllers/auth.controller.js
+++ services/auth-service/src/controllers/auth.controller.js
@@ -1,93 +1,49 @@
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const {
+  registerUser,
+  loginUser,
+} = require("../services/auth.service");
+
 const register = async (req, res) => {
   try {
-    const { name, email, password, role } = req.body;
-
-    // Check if email already exists
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({
-        success: false,
-        message: "User already exists",
-      });
-    }
-
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create user
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-      role,
-    });
+    const user = await registerUser(req.body);
 
     res.status(201).json({
       success: true,
       message: "User Registered Successfully",
       data: user,
     });
+
   } catch (error) {
-    res.status(500).json({
+
+    res.status(400).json({
       success: false,
       message: error.message,
     });
+
   }
 };
+
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
 
-    // Find user
-    const user = await User.findOne({ email });
+    const result = await loginUser(req.body);
 
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-
-    // Compare password
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(401).json({
-        success: false,
-        message: "Invalid Credentials",
-      });
-    }
-
-    // Generate JWT
-    const token = jwt.sign(
-      {
-        id: user._id,
-        role: user.role,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "1d",
-      },
-    );
-    const userResponse = user.toObject();
-    delete userResponse.password;
     res.status(200).json({
       success: true,
       message: "Login Successful",
-      token,
-      user,
+      ...result,
     });
+
   } catch (error) {
-    res.status(500).json({
+
+    res.status(400).json({
       success: false,
       message: error.message,
     });
+
   }
 };
+
 module.exports = {
   register,
   login,
