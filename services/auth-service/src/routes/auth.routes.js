4b384baa26d9e452db4a96b75ee288f6775bb39c
Disha Agrawal <dishaagrawal9568@gmail.com>
2026-07-30 16:08:33 +0530

    auth routes

--- services/auth-service/src/routes/auth.routes.js
+++ services/auth-service/src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const router = express.Router();
-
+const authenticateUser = require("../middleware/auth.middleware");
+const authorizeRoles = require("../middleware/authorize.middleware");
 const { register, login } = require("../controllers/auth.controller");
 
 router.get("/health", (req, res) => {
@@ -13,4 +14,59 @@ router.get("/health", (req, res) => {
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/profile", authenticateUser, (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Profile fetched successfully",
+        user: req.user
+    });
+
+});
+router.get(
+  "/admin",
+  authenticateUser,
+  authorizeRoles("admin"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome Admin",
+    });
+  }
+);
+
+router.get(
+  "/faculty",
+  authenticateUser,
+  authorizeRoles("faculty"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome Faculty",
+    });
+  }
+);
+
+router.get(
+  "/student",
+  authenticateUser,
+  authorizeRoles("student"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome Student",
+    });
+  }
+);
+router.get(
+  "/admin-faculty",
+  authenticateUser,
+  authorizeRoles("admin", "faculty"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome Admin/Faculty",
+    });
+  }
+);
 module.exports = router;
