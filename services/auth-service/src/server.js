b1dddf7abcc49fa0dea534c56f686aee724eb455
Disha Agrawal <dishaagrawal9568@gmail.com>
2026-07-30 16:53:11 +0530

    morgan and winston

--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const connectDB = require("./config/db");
 
@@ -13,6 +14,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 app.use("/api/auth", authRoutes);
 app.use(errorHandler);
 
