5277baa67f178fe556b3b2b50ff6d26d25abc735
Disha Agrawal <dishaagrawal9568@gmail.com>
2026-08-01 00:21:38 +0530

    routes changes for swagger

--- services/student-service/src/routes/student.routes.js
+++ services/student-service/src/routes/student.routes.js
@@ -2,19 +2,101 @@ const express = require("express");
 
 const router = express.Router();
 
-const { create } = require("../controllers/student.controller");
+const {
+  create,
+  getAll,
+  getById,
+  update,
+  remove,
+} = require("../controllers/student.controller");
+
+const validate = require("../middleware/validation.middleware");
 
 const {
   createStudentValidation,
 } = require("../validations/student.validation");
 
-const validate = require("../middleware/validation.middleware");
+/**
+ * @swagger
+ * /api/students:
+ *   post:
+ *     summary: Create Student
+ *     tags:
+ *       - Students
+ *     responses:
+ *       201:
+ *         description: Student created successfully
+ */
+router.post("/", createStudentValidation, validate, create);
 
-router.post(
-  "/",
-  createStudentValidation,
-  validate,
-  create
-);
+/**
+ * @swagger
+ * /api/students:
+ *   get:
+ *     summary: Get All Students
+ *     tags:
+ *       - Students
+ *     responses:
+ *       200:
+ *         description: Students fetched successfully
+ */
+router.get("/", getAll);
+
+/**
+ * @swagger
+ * /api/students/{id}:
+ *   get:
+ *     summary: Get Student By ID
+ *     tags:
+ *       - Students
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Student fetched successfully
+ */
+router.get("/:id", getById);
+
+/**
+ * @swagger
+ * /api/students/{id}:
+ *   put:
+ *     summary: Update Student
+ *     tags:
+ *       - Students
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Student updated successfully
+ */
+router.put("/:id", update);
+
+/**
+ * @swagger
+ * /api/students/{id}:
+ *   delete:
+ *     summary: Delete Student
+ *     tags:
+ *       - Students
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Student deleted successfully
+ */
+router.delete("/:id", remove);
 
 module.exports = router;
